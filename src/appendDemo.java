9d70d6161f92b411117626fa020ff9d2a8a53acf
nIGHTVEn <vitaliy.beyar@gmail.com>
2018-03-13 11:37:14 +0200

    тестим

--- src/appendDemo.java
+++ src/appendDemo.java
@@ -2,7 +2,7 @@ public class appendDemo {
     //Демонстрация применения append().
     public static void main(String[] args) {
         String s;
-        int a = 42;
+        int a = 423;
         StringBuffer sb = new StringBuffer(40);
         s = sb.append("a = ").append(a).append("!").toString();
         System.out.println(s);
