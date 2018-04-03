c8fa9ee400925b2cb9043576d2c1fd53e737ca63
nIGHTVEn <vitaliy.beyar@gmail.com>
2018-04-03 16:56:34 +0300

    дз11 приступаем к дз 11

--- src/HW9/MyList.java
+++ src/HW9/MyList.java
@@ -2,14 +2,12 @@ package HW9;
 // Опционально *сделать MyList параметризованным
 public interface MyList {
      void addList(Object e);
-
-
-    void clearList();
-    boolean removeList(Object o);
-    Object[] listToArray();
-    int sizeList();
-    boolean containsList(Object o);
-    boolean containsAllList(MyList c);
+     void clearList();
+     boolean removeList(Object o);
+     Object[] listToArray();
+     int sizeList();
+     boolean containsList(Object o);
+     boolean containsAllList(MyList c);
 
 }
 
