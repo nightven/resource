6d97c9f97582f3e62c97b498856e98729c6f7b22
nIGHTVEn <vitaliy.beyar@gmail.com>
2018-04-13 21:30:03 +0300

    Добавил CopyOnWriteArrayList из ДЗ 12

--- src/HW12/CopyOnWriteArrayListExample.java
+++ src/HW12/CopyOnWriteArrayListExample.java
@@ -2,21 +2,43 @@ package HW12;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
-public class CopyOnWriteArrayList {
-    public boolean add(String e) {
+public class CopyOnWriteArrayListExample {
 
 
-            Object[] elements = getArray();
-            int len = elements.length;
-            Object[] newElements = Arrays.copyOf(elements, len + 1);
-            newElements[len] = e;
-            setArray(newElements);
-            return true;
+    public static void main(String[] args) {
+        /*
+        Создадим новую колекцию CopyOnWriteArrayList на основе интерфейса List
+        и занесемт туда несколько значений типа Integer
+         */
+        List<Integer> list = new CopyOnWriteArrayList<>(new Integer[]{1, 5, 8, 9});
+        /*
+        При создании итератора для даной коллекции у нас создаеться копия даной колекции
+        с которой и будет работать итератор
+         */
+        Iterator<Integer> iterator = list.iterator();
+        /*
+        Поэтому внесение нового елемента в эту колекцию никак не отобразится, так как итератор раотает с копией не
+        модифицированой колекции
+         */
+        list.add(10); // етот елемент не будет выведен
+
+        while (iterator.hasNext()) {      //проверим, что колекция не изменилась
+            System.out.println(iterator.next());
+        }
+        System.out.println();
+        /*
+        Для того чтобы вывести модифицированую коллекцию нам нужен новый итератор
+         */
+        Iterator<Integer> iterator2 = list.iterator();
+
+        while (iterator2.hasNext()) {  //теперь выведем модифицированую коллекцию
+            System.out.println(iterator2.next());
+        }
+
     }
-    ArrayList<String> auto = new ArrayList<>();
-        auto.add("car Audi");
-        auto.add("car Nissan");
-        auto.add("truck Volvo");
-        auto.add("truck Renault");
+
 }
